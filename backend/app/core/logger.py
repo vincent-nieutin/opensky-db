d3d27f13fd1b96e781cbd1ec8b223057fd146652
vincent-nieutin <vincent.nieutin@yahoo.fr>
2025-09-09 11:58:48 +0200

    change file logging to when on dev only

--- backend/app/core/logger.py
+++ backend/app/core/logger.py
@@ -1,5 +1,7 @@
+import os
+import sys
 import logging
-from app.core.config import LOG_FILE, LOG_LEVEL
+from app.core.config import LOG_FILE, LOG_LEVEL, ENVIRONMENT
 
 log_level = getattr(logging, LOG_LEVEL, logging.INFO)
 
@@ -12,17 +14,20 @@ formatter = logging.Formatter(
     "%(asctime)s [%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
 )
 
-# File handler
-file_handler = logging.FileHandler(LOG_FILE)
-file_handler.setLevel(log_level)
-file_handler.setFormatter(formatter)
-
 # Console handler
-console_handler = logging.StreamHandler()
+console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setLevel(log_level)
 console_handler.setFormatter(formatter)
 
+# File handler if on dev
+if ENVIRONMENT == "development":
+    os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
+    file_handler = logging.FileHandler(LOG_FILE)
+    file_handler.setLevel(log_level)
+    file_handler.setFormatter(formatter)
+
 # Add both handlers to the logger if not already configured
 if not logger.hasHandlers():
-    logger.addHandler(file_handler)
     logger.addHandler(console_handler)
+    if ENVIRONMENT == "development":
+        logger.addHandler(file_handler)
